808a71d1e7b623dc59bad66857557dac4584e2de
Rodolfo Chan <rodolfochan2910@gmail.com>
2024-10-29 10:28:52 -0600

    agregar corshe coreccion2

--- gestion_talleres/settings.py
+++ gestion_talleres/settings.py
@@ -67,7 +67,7 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
-ORS_ALLOW_ALL_ORIGINS = True
+CORS_ALLOW_ALL_ORIGINS = True
 ROOT_URLCONF = 'gestion_talleres.urls'
 
 TEMPLATES = [
