83bd4f95633d7e83a4fdefd0fab2ce12b225974a
Rodolfo Chan <rodolfochan2910@gmail.com>
2024-11-04 16:12:52 -0600

    agregar tabla rubrica_reportes migratev3

--- talleres/models.py
+++ talleres/models.py
@@ -40,7 +40,7 @@ class UsuarioAdmin(models.Model):
 # Modelo para la tabla Alumnos
 class Alumno(models.Model):
     #id_alumno = models.AutoField(primary_key=True)
-    id_alumno = models.IntegerField(primary_key=True, default=1)
+    id_alumno = models.IntegerField(primary_key=True)
     matricula_alumno = models.CharField(
         max_length=8,
         validators=[RegexValidator(r'^\d{8}$', 'La matrícula debe tener exactamente 8 dígitos.')],
