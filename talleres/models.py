0650f9b7a09937340295a273fc54a239d8aaa859
Rodolfo Chan <rodolfochan2910@gmail.com>
2024-11-04 21:31:21 -0600

    correr test8

--- talleres/models.py
+++ talleres/models.py
@@ -42,7 +42,7 @@ class Alumno(models.Model):
     #id_alumno = models.AutoField(primary_key=True)
     id_alumno = models.AutoField(primary_key=True)  # Use AutoField for an identity column
 
-    matricula = models.CharField(
+    matricula_alumno = models.CharField(
         max_length=8,
         validators=[RegexValidator(r'^\d{8}$', 'La matrícula debe tener exactamente 8 dígitos.')],
         unique=True
